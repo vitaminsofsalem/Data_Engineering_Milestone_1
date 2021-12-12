c5f90580d6bd5048eadcd72152e5cf9f536eb04a
fakhry tatanaki <fakhry8821@icloud.com>
2021-12-12 19:10:03 +0200

    code simplification and adjustments

--- DataProcTools/DataCleaning/MissingValues.py
+++ DataProcTools/DataCleaning/MissingValues.py
@@ -1,6 +1,24 @@
 import pandas as pd
 
+def height_imputer(dataset, average_height):
+    for row in dataset.index:    
+        if((dataset['Height'].isna()[row])):        
+            NOC_value = dataset['NOC'][row]
+            height_per_NOC = average_height.loc[NOC_value, 'Height']
+            dataset['Height'].fillna(height_per_NOC, inplace=True)
+            
+def weight_imputer(dataset, average_weight):
+    for row in dataset.index:
+        if((dataset['Weight'].isna()[row])):
+            NOC_value = dataset['NOC'][row]
+            weight_per_NOC = average_weight.loc[NOC_value, 'Weight']
+            dataset['Weight'].fillna(weight_per_NOC, inplace=True)    
 
+def age_imputer(dataset, average_age): 
+    dataset['Age'].fillna(average_age, inplace=True)
+
+def medal_imputer(dataset):
+    dataset['Medal'].fillna('No_Medal', inplace=True)
 
 def get_nan_values_percentage(df):
     info = {}
